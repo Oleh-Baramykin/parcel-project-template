e67ac096f0aba024c139a4ef15ea3275d7692a17
Oleh Baramykin <olegix7891@ukr.net>
2023-01-23 21:48:35 +0200

    ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
 const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const videoPlayerKey = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(videoPlayerKey, data.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(videoPlayerKey))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
